cc932ce4c0c5d68c2fd429a806c696de0865229a
vitorregisr <vitorregisrr@gmail.com>
2020-12-15 00:29:13 -0300

    Add faq with custom scroll

--- js/main.js
+++ js/main.js
@@ -5,34 +5,8 @@
     new WOW().init();
     $('.lazy').lazyload();
 
-    //scroll top
-    var scrollTopBtn = $('.scroll-top');
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 300) {
-            scrollTopBtn.addClass('show');
-        } else {
-            scrollTopBtn.removeClass('show');
-        }
-
-        if ($(window).scrollTop() > 200) {
-            $('#home-banner-more').css("opacity", 1 - $(window).scrollTop() / ($('#home-banner-more').height() + 400) );
-        } else {
-            $('#home-banner-more').css("opacity", 1);
-        }
-    });
-
-     //Smooth Scrooling
-     $('a[href*=\\#]:not([href$=\\#])').click(function () {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href'))
-                .offset()
-                .top - 300
-        }, 500);
-    });
-
+    // CustomScrollbar
+    
     scrollTopBtn.on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
