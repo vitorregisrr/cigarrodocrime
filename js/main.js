db6816bfce57b77d202cd59630043d5a949b0698
vitorregisr <vitorregisrr@gmail.com>
2020-12-18 21:14:15 -0300

    Misc improves

--- js/main.js
+++ js/main.js
@@ -8,47 +8,77 @@
     $(document).ready(function () {
         // pagepilling
         $('#pagepiling').pagepiling({
-            sectionsColor: ['#000', '#fff', '#000', '#fff', '#F8F8F8', '#000', '#F8F8F8', '#000', '#fff', '#fff'],
+            sectionsColor: [
+                '#000',
+                '#fff',
+                '#000',
+                '#fff',
+                '#F8F8F8',
+                '#000',
+                '#F8F8F8',
+                '#000',
+                '#fff',
+                '#fff'
+            ],
             scrollBar: true,
-            anchors: ['menu-home', 'menu-barato', 'menu-crime', 'menu-numeros', 'menu-documentario', 'menu-ciclo', 'menu-contrabando', 'menu-episodios', 'menu-faq', 'menu-sobre'],
-            onLeave: function(anchorLink, index){
-                $(`#pagepiling > .section:nth-child(${index}) .has-animation`).each(function( index, element ) {
-                    var animation = $(element).attr('data-animation');
-                    var duration = $(element).attr('data-duration') || '1s';
-                    var delay = $(element).attr('data-delay') || '0';
+            anchors: [
+                'menu-home',
+                'menu-barato',
+                'menu-crime',
+                'menu-numeros',
+                'menu-documentario',
+                'menu-ciclo',
+                'menu-contrabando',
+                'menu-episodios',
+                'menu-faq',
+                'menu-sobre'
+            ],
+            touchSensitivity: 1000,
+            onLeave: function (anchorLink, index) {
+                $(`#pagepiling > .section:nth-child(${index}) .has-animation`)
+                    .each(function (index, element) {
+                        var animation = $(element).attr('data-animation');
+                        var duration = $(element).attr('data-duration') || '1s';
+                        var delay = $(element).attr('data-delay') || '0';
 
-                    $(element).removeClass('animated')
-                    $(element).removeClass(animation);
-                    $(element).removeClass(animation);
-                    $(element).attr('style', function(i,s) { return (s || '') + `animation-delay: ${delay} !important;` });
-                    $(element).attr('style', function(i,s) { return (s || '') + `animation-duration: ${duration} !important;` });
-                    
-                    setTimeout(() => {                    
-                        $(element).addClass('animated')
-                        $(element).addClass(animation);
-                    }, 20);
-                });
+                        $(element).removeClass('animated')
+                        $(element).removeClass(animation);
+                        $(element).removeClass(animation);
+                        $(element).attr('style', function (i, s) {
+                            return (s || '') + `animation-delay: ${delay} !important;`
+                        });
+                        $(element).attr('style', function (i, s) {
+                            return (s || '') + `animation-duration: ${duration} !important;`
+                        });
+
+                        setTimeout(() => {
+                            $(element).addClass('animated')
+                            $(element).addClass(animation);
+                        }, 20);
+                    });
             },
-            afterLoad: function(anchor, index){
-                if(index === 7){
+            afterLoad: function (anchor, index) {
+                if (index === 7) {
                     $('#pp-nav').addClass('to-bottom')
-                }else{
+                } else {
                     $('#pp-nav').removeClass('to-bottom')
                 }
             }
         });
-        $.fn.pagepiling.setAllowScrolling(true);
+        $
+            .fn
+            .pagepiling
+            .setAllowScrolling(true);
     });
 
-
-    $('.episodios__tabs-item').click(function(){
+    $('.episodios__tabs-item').click(function () {
         $('.episodios__tabs-item').removeClass('active');
     });
-    $('.episodios__tabs-item').hover(function(){
+    $('.episodios__tabs-item').hover(function () {
         $('.episodios__tabs-item').removeClass('active');
     });
 
-    if(window.innerWidth > 768){
+    if (window.innerWidth > 768) {
         $('.episodios__tabs-item').removeClass('active');
     }
 
